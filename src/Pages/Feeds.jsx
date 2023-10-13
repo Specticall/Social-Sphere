feadcf0f3ee1a134724c92925f95e1e8d5cfdd22
Specticall <josephyusmita@gmail.com>
2023-10-13 13:26:05 +0700

    added static user profile

--- src/Pages/Feeds.jsx
+++ src/Pages/Feeds.jsx
@@ -42,7 +42,9 @@ export default function Feeds({
           ))}
         </div>
       </div>
-      <div className="right">right</div>
+      <div className="right">
+        <UserProfile />
+      </div>
     </div>
   );
 }
@@ -118,5 +120,54 @@ function StoryCards({ image: IMG_URL, caption, poster }) {
         <p>{poster}</p>
       </summary>
     </div>
+  );
+}
+
+function UserProfile() {
+  return (
+    <article className="user-profile">
+      <div className="background"></div>
+      <div className="user-profile__wrapper">
+        <button className="user-profile__logout-btn">
+          <i className="bx bx-log-out"></i>
+          Log out
+        </button>
+        <div className="user-profile__pfp"></div>
+        <h2>Sarah Wilson</h2>
+        <p className="user-profile__profession">
+          Software Engineer
+        </p>
+        <p className="user-profile__tag">@sarahwls</p>
+        <article className="user-profile__about-me">
+          <h3>About me</h3>
+          <p>
+            &quot;Hi, I&apos;m Sarah – a software engineer
+            by day, photographer by night, weaving code and
+            taste into my digital world.&quot;
+          </p>
+        </article>
+        <article className="user-profile__status">
+          <h3>Status</h3>
+          <ul className="status-list">
+            <li>
+              <i className="bx bx-map"></i>
+              Jakarta, Indonesia
+            </li>
+            <li>
+              <i className="bx bx-user"></i>
+              Undergrad, Student
+            </li>
+            <li>
+              <i className="bx bx-heart"></i>
+              Single
+            </li>
+            <li>
+              <i className="bx bx-calendar"></i>
+              21 Years old
+            </li>
+          </ul>
+        </article>
+      </div>
+    </article>
   );
 }
